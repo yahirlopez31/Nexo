4191846b69fa8d0c603c0b19ea7205758177187a
Liam-Monke <liam.mortera1789@alumnos.udg.mx>
2025-08-21 17:35:14 -0600

    blocks

--- nexo.py
+++ nexo.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 nexoApp = Flask(__name__) 
 
 @nexoApp.route('/')
 def home():
-    return '<h1> Hola hola!</h1>' 
+    return render_template('home.html')
 
 if __name__ == '__main__':
     nexoApp.run(debug=True,port=7777)
